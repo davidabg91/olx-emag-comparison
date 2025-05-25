ad11759de46a3a9b1c95f5c6fe27206c22a019f5
Deyvid Dimitrov <davida1991@gmail.com>
2025-05-25 20:21:25 +0300

    одобрена стабилност на приложението

--- build.sh
+++ build.sh
@@ -4,11 +4,19 @@ set -o errexit
 
 # Инсталиране на Chrome
 apt-get update
-apt-get install -y wget gnupg
+apt-get install -y wget gnupg2
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 apt-get update
 apt-get install -y google-chrome-stable
+
+# Инсталиране на ChromeDriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
 
 # Инсталиране на Python зависимости
 pip install -r requirements.txt 
