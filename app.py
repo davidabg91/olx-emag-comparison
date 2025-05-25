14e172b32fb9a4daae32fc18f6c5a4608aa8c0f5
Deyvid Dimitrov <davida1991@gmail.com>
2025-05-25 20:17:03 +0300

    обавени настройки за Render

--- app.py
+++ app.py
@@ -172,4 +172,5 @@ if __name__ == '__main__':
     scraper_thread.start()
     
     # Стартиране на сървъра с публичен хост
-    app.run(host='0.0.0.0', port=5000, debug=False) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
