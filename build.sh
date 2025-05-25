41a209bcb27aee6ea1cd8905412619020f66fdaa
Deyvid Dimitrov <davida1991@gmail.com>
2025-05-25 20:23:04 +0300

    простени зависимости и build процес

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Инсталиране на Chrome
 apt-get update
-apt-get install -y wget gnupg2
+apt-get install -y wget gnupg2 unzip
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 apt-get update
@@ -12,11 +12,10 @@ apt-get install -y google-chrome-stable
 
 # Инсталиране на ChromeDriver
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
-CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+wget -q "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 
 # Инсталиране на Python зависимости
-pip install -r requirements.txt 
+pip install --no-cache-dir -r requirements.txt 
