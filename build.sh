be633dad9424acf6f854bc61b07ec662339cc14b
Deyvid Dimitrov <davida1991@gmail.com>
2025-05-25 20:18:11 +0300

    обавени настройки за Chrome в Render

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Инсталиране на Chrome
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Инсталиране на Python зависимости
+pip install -r requirements.txt 
